98cd5237e87b3ccd571fa41eeaf9fd3c2651f718
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-07-07 08:22:09 +0200

    view for route testing

---
+++ spec/controllers/todos_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe TodosController do
+
+  describe "GET #index" do
+    it "assigns @todos" do
+      todo = Todo.create( title: "Shopping" )
+
+      get :index
+
+      expect(assigns(:todos)).to eq([todo])
+      expect(response).to render_template("index")
+    end
+  end
+
+
+
+end
---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Todo do
+  describe "validations" do
+    it "is invalid without a title" do
+      todo = Todo.new(title: "")
+      todo.valid?
+      expect(todo.errors).to have_key(:title)
+    end
+  end
+end
